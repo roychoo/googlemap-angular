e826d6e1f4ea56e9c59c9033e040acc6c7603ebc
Roy Choo <rchoo@paypal.com>
2012-07-04 18:53:12 +0800

    adding the map controller

--- app/js/app.js
+++ app/js/app.js
@@ -4,8 +4,8 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: loginController});
+    $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: LoginController});
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
+    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MapController});
     $routeProvider.otherwise({redirectTo: '/login'});
   }]);
